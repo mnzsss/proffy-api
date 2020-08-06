fb8044017a2c278920c3541591d3b7483e23ffc0
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2020-08-06 12:08:00 -0300

    chore: isolate routes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({
-    ok: true,
-  });
-});
+app.use(routes);
 
 app.listen(3333, () =>
   console.log(`Server starts on port http://localhost:3333`),
